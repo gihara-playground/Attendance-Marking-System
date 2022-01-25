921194cacb28a4da9a1a2739d4914f17047fe175
Gihara Wicktramatunge <giharanw@gmail.com>
2022-01-25 15:06:20 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Attendance Marking System
+A simple to system mark student attendance.
+### Version
+0.0.1       
+### License
+All Rights Reserved.<br>Licensed to [MIT](LICENSE.txt) license.
---
+++ src/AppInitializer.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(this.getClass().getResource("/view/LoginFormAdmin.fxml.fxml"));
+        Scene mainScene = new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Attendance Marking System - Login");
+        primaryStage.setResizable(false);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
